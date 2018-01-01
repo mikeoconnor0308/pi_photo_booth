959628019d1c0b85dc62fd26ba86288e18781683
Mike O'Connor <mikeoconnor0308@gmail.com>
2018-01-01 19:47:05 +0000

    started making filter screen

--- gamescreen.py
+++ gamescreen.py
@@ -26,26 +26,37 @@ class PhotoboothGame:
         self.screen.fill(self.black)
         pygame.display.update()
 
+    def position_in_center(self, surface):
+        yadjust = -50
+        rect = surface.get_rect()
+        rect.centerx = self.screen.get_rect().centerx
+        rect.centery = self.screen.get_rect().centery + yadjust
+        self.screen.blit(surface, rect)
+
     def reviewing_phase(self):
         self.phase = GamePhase.REVIEWING
         self.screen.fill(self.white)
-        img = pygame.image.load("{}/weddingshot.jpg".format(self.photo_dir)).convert()
+        self.photo = pygame.image.load("{}/weddingshot.jpg".format(self.photo_dir)).convert()
 
         # scale and adjust image, then render it.
-        w, h = img.get_size()
-        scale = 0.75
-        yadjust = -50
-        img = pygame.transform.scale(img, (int(w * scale), int(h * scale)))
-        rect = img.get_rect()
-        rect.centerx = self.screen.get_rect().centerx
-        rect.centery = self.screen.get_rect().centery + yadjust
-        self.screen.blit(img, rect)
+        w, h = self.photo.get_size()
+        scale = 0.9
 
+        self.photo = pygame.transform.scale(self.photo, (int(w * scale), int(h * scale)))
+        self.position_in_center(self.photo)
+        rect = self.photo.get_rect()
+
+        # Render text beneath photo
         textsurface = self.font.render('Upload photo?', False, (120, 120, 120))
         textcoords = (rect.left, rect.bottom + 10)
         self.screen.blit(textsurface, textcoords)
 
         pygame.display.update()
+
+    def filter_phase(self):
+        self.phase = GamePhase.FILTERING
+        self.screen.fill(self.white)
+        self.position_in_center(self.photo)
 
     def __init__(self, photo_path):
         # set up game screen
--- main.py
+++ main.py
@@ -1,37 +1,20 @@
 import time
 import picamera
+import instagramfilters.instagram_filters
+from instagramfilters.instagram_filters.filters import *
+import shutil
+import inspect
+import os
 import RPi.GPIO as GPIO
 import atexit
 import os
 import sys
 import pygame
-from gamescreen import PhotoboothGame
-
-#######################
-### Variables Config ##
-#######################
-
-big_button = 5
-led = 26
-stop_button = 4
-focus = 16
-shoot = 12
-
-####################
-### Other Config ###
-####################
-
-GPIO.setmode(GPIO.BCM)
-GPIO.setup(led, GPIO.OUT)
-GPIO.setup(focus, GPIO.OUT)
-GPIO.setup(shoot, GPIO.OUT)
-GPIO.setup(big_button, GPIO.IN)
-GPIO.setup(stop_button, GPIO.IN)
-GPIO.setup(32, GPIO.OUT, initial=False)
+from gamescreen import *
+import threading
 
 camera = picamera.PiCamera()
-
-camera.resolution = (1024, 768)
+camera.resolution = (640, 640)
 camera.annotate_text_size = 70
 camera.annotate_background = picamera.Color('white')
 camera.annotate_foreground = picamera.Color('black')
@@ -41,6 +24,9 @@ camera.exposure_compensation = 25
 photo_dir = "photos"
 if not os.path.exists(photo_dir):
     os.makedirs(photo_dir)
+filter_dir = os.path.join(photo_dir, "filtered")
+if not os.path.exists(filter_dir):
+    os.makedirs(filter_dir)
 
 game = PhotoboothGame(photo_dir)
 
@@ -55,28 +41,37 @@ def countdown(t):
     camera.annotate_text = ''
 
 
+def generate_filters():
+    # bit of a hack, might be slow. just run all the filters.
+    for name, obj in inspect.getmembers(instagramfilters.instagram_filters.filters):
+        if inspect.isclass(obj):
+            filename = os.path.join("photos", "filtered", name + ".jpg")
+            shutil.copyfile("{}/weddingshot.jpg".format(photo_dir), filename)
+            f = obj(filename)
+            f.apply()
+
+
 def take_photo():
     now = time.strftime("%Y-%m-%d-%H:%M:%S")
     try:
-        GPIO.output(led, 0)
-        GPIO.output(focus, 1)
         camera.annotate_text = '   Get ready!   '
         print('Get ready!')
         # TODO ready time
         # countdown(1)
         print('Taking photo')
         camera.annotate_text = ''
-        GPIO.output(focus, 0)
         # switch game to not showing anything.
         game.capturing_phase()
         camera.stop_preview()
-        GPIO.output(shoot, 1)
         camera.capture("{}/weddingshot.jpg".format(photo_dir))
         time.sleep(1)
-        GPIO.output(shoot, 0)
 
     finally:
         display_photo()
+        # generate filters
+        print('Generating filters')
+        generate_filters()
+        print('Done generating filters')
 
 
 def display_photo():
@@ -88,15 +83,8 @@ def display_photo():
         game.previewing_phase()
         camera.start_preview()
 
-    finally:
-        GPIO.output(led, 1)
 
-
-GPIO.output(led, 1)
-GPIO.output(32, 0)
-print('Running')
 camera.start_preview()
-
 while True:
 
     # TODO wire up button events
@@ -109,7 +97,16 @@ while True:
             if event.key == pygame.K_ESCAPE:
                 pygame.quit()
 
-            if event.key == pygame.K_SPACE:
-                print("Taking photo!")
-                time.sleep(0.2)
-                take_photo()
+            if game.phase == GamePhase.PREVIEWING:
+                if event.key == pygame.K_SPACE:
+                    print("Taking photo!")
+                    time.sleep(0.2)
+                    take_photo()
+            elif game.phase == GamePhase.REVIEWING:
+                if event.key == pygame.K_SPACE:
+                    print("Moving to filter screen!")
+                    game.filter_phase()
+                if event.key == pygame.K_BACKSPACE:
+                    print("Back to preview")
+                    game.previewing_phase()
+
